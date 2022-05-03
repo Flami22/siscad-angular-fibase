341f2f848704220a972f32f826c6afadfdd79424
João Henrique <104780628+JoaoHenriqueAxyma@users.noreply.github.com>
2022-05-03 01:42:41 -0300

    fix rendering forms cadastro

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,28 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.sass']
+  styleUrls: ['./cadastro.component.sass'],
 })
 export class CadastroComponent implements OnInit {
-formProduct: FormGroup;
+  formProduct: FormGroup;
   constructor(private formBuilder: FormBuilder, private crud: CrudService) {
+    console.log('salve');
     this.formProduct = formBuilder.group({
-      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      preco: ['', Validators.compose([Validators.required])],
-      qtde:[1, , Validators.compose([Validators.required, Validators.min(1)])]
-    })
+      nome: [''],
+      preco: [''],
+      qtde: [1],
+    });
+    console.log('deu ruim');
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   saveProduct() {
-    if(this.formProduct.valid) {
-      this.crud.create(this.formProduct.value)
+    if (this.formProduct.valid) {
+      this.crud.create(this.formProduct.value);
     }
-}
+  }
 }
